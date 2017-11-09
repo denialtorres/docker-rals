2abcb244c47917fd326f577d8db35c781339946a
Daniel <denial.torres@gmail.com>
2017-11-09 10:27:09 -0600

    secret variables

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-puts "HOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
-
 
 environment 'production'
 threads 0,5
@@ -8,7 +6,6 @@ workers 3
 preload_app!
 
 on_worker_boot do
-  puts "HOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
   end
